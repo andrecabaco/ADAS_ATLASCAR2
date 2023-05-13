963ea9700bfc6b0d286a0a90c6f697434dd24c22
andrecabaco <andrecabaco@ua.pt>
2023-05-13 18:07:34 +0100

    Updating lidar subscriber script

--- lidar2.py
+++ lidar2.py
@@ -1,72 +1,67 @@
 #!/usr/bin/env python3
+import argparse
+import math
+from functools import partial
 
 import rospy
-from sensor_msgs.msg import LaserScan, PointCloud2
-import sensor_msgs.point_cloud2 as pc2
-from math import cos, sin, radians
+from sensor_msgs.msg import LaserScan, PointCloud2, PointField
+from std_msgs.msg import String, Header
+from colorama import Fore, Style
+from sensor_msgs import point_cloud2
 
 
-def lidar1_callback(msg, args):
-    lidar1_frame_id, lidar1_data, pub_point_cloud = args
-    lidar1_data = msg
-    merge_lidar_data_and_publish(lidar1_data, lidar2_data, lidar1_frame_id, pub_point_cloud)
+def msgReceivedCallback(msg, publisher):
+
+    rospy.loginfo("I received a new laser scan msg")
+
+    # inspiration from https://gist.github.com/lucasw/ea04dcd65bc944daea07612314d114bb
+    # sensor_msgs.msg PointCloud2 is the actual serialized message, but to manipulate we use the sensor_msgs
+    # point_cloud2 data structure.
+    header = Header(seq=msg.header.seq, stamp=msg.header.stamp, frame_id=msg.header.frame_id)
+    fields = [PointField('x', 0, PointField.FLOAT32, 1),
+              PointField('y', 4, PointField.FLOAT32, 1),
+              PointField('z', 8, PointField.FLOAT32, 1)]
+
+    # Create a list of xyz coords
+    points = []
+    for idx in range(0, len(msg.ranges)):
+        alpha = msg.angle_min + idx * msg.angle_increment
+        r = msg.ranges[idx]
+
+        x = r * math.cos(alpha)
+        y = r * math.sin(alpha)
+        z = 0
+        points.append([x,y,z])
+
+    pc2 = point_cloud2.create_cloud(header, fields, points)  # create point_cloud2 data structure
+
+    publisher.publish(pc2)
 
 
-def lidar2_callback(msg, args):
-    lidar2_frame_id, lidar2_data, pub_point_cloud = args
-    lidar2_data = msg
-    merge_lidar_data_and_publish(lidar1_data, lidar2_data, lidar2_frame_id, pub_point_cloud)
+    
+def main():
 
+    # ------------------------------------
+    # Initialization 
+    # ------------------------------------
 
-def merge_lidar_data_and_publish(lidar1_data, lidar2_data, frame_id, pub_point_cloud):
-    if lidar1_data is None or lidar2_data is None:
-        return
+    # Initialization of a ros node
+    rospy.init_node('lidar_subscriber', anonymous=False)
 
-    # Convert lidar data to PointCloud2 messages
-    point_cloud1 = convert_to_point_cloud(lidar1_data, frame_id)
-    point_cloud2 = convert_to_point_cloud(lidar2_data, frame_id)
+    publisher = rospy.Publisher('~point_cloud', PointCloud2, queue_size=1)
 
-    # Merge the two PointCloud2 messages
-    merged_point_cloud = merge_point_clouds(point_cloud1, point_cloud2)
+    # Init the subscriber
+    subscriber_left = rospy.Subscriber('/left_laser/scan', LaserScan, partial(msgReceivedCallback, publisher=publisher))
+    subscriber_right = rospy.Subscriber('/right_laser/scan', LaserScan, partial(msgReceivedCallback, publisher=publisher))
 
-    # Publish the merged PointCloud2 message
-    pub_point_cloud.publish(merged_point_cloud)
-
-
-def convert_to_point_cloud(lidar_data, frame_id):
-    # Convert lidar data to PointCloud2 message
-    point_cloud = pc2.create_cloud_xyz32(header=rospy.Header(frame_id=frame_id),
-                                         points=[[reading * cos(radians(angle)),
-                                                   reading * sin(radians(angle)),
-                                                   0.0] for angle, reading in enumerate(lidar_data.ranges)])
-
-    return point_cloud
-
-
-def merge_point_clouds(point_cloud1, point_cloud2):
-    # Merge two PointCloud2 messages into one
-    merged_point_cloud = pc2.read_points(point_cloud1) + pc2.read_points(point_cloud2)
-
-    # Convert the merged points to a new PointCloud2 message
-    new_point_cloud = pc2.create_cloud_xyz32(header=rospy.Header(frame_id="merged_lidar"),
-                                             points=list(merged_point_cloud))
-
-    return new_point_cloud
-
-
-if __name__ == "__main__":
-    rospy.init_node("lidar_merger")
-
-    lidar1_frame_id = rospy.get_param("~lidar1_frame_id", "lidar1")
-    lidar2_frame_id = rospy.get_param("~lidar2_frame_id", "lidar2")
-    lidar1_data = None
-    lidar2_data = None
-
-    pub_point_cloud = rospy.Publisher("~merged_point_cloud", PointCloud2, queue_size=10)
-
-    rospy.Subscriber("~lidar1_scan", LaserScan, lidar1_callback, (lidar1_frame_id, lidar1_data, pub_point_cloud))
-    rospy.Subscriber("~lidar2_scan", LaserScan, lidar2_callback, (lidar2_frame_id, lidar2_data, pub_point_cloud))
-
+    # ------------------------------------
+    # Execution 
+    # ------------------------------------
     rospy.spin()
 
+    # ------------------------------------
+    # Termination 
+    # ------------------------------------
 
+if __name__ == '__main__':
+    main()
